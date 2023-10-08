e2f6413c613317c77fe89bc88798fbbfabdd97c4
Qammarbhat <qammarbhat@bgsbu.ac.in>
2023-10-08 11:02:16 +0530

    code update

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
+# Import necessary libraries
 import streamlit as st
-from prediction import predict_stock
-import langchain
-from prediction import predict_stock
+from prediction import predict_stock  # Import the predict_stock function from prediction.py
 
+# Set the title of the Streamlit app
 st.title("Stock Price Predictor")
 
+# Create input fields for user to enter stock data
+open_price = st.number_input("Open Price:", step=0.01)  # Input for Open Price
+high_price = st.number_input("High Price:", step=0.01)  # Input for High Price
+low_price = st.number_input("Low Price:", step=0.01)  # Input for Low Price
+volume = st.number_input("Volume:", step=0.01)  # Input for Volume
 
-# Creating input fields
-open_price = st.number_input("Open Price:", step = 0.01)
-high_price = st.number_input("High Price:", step = 0.01)
-low_price = st.number_input("Low Price:", step = 0.01)
-volume = st.number_input("Volume:", step = 0.01)
-
+# Check if the "Predict" button is clicked
 if st.button("Predict"):
-  future_price = predict_stock(open_price, high_price, low_price, volume)
-  st.write( future_price) 
-
+    # Call the predict_stock function to get the future stock price
+    future_price = predict_stock(open_price, high_price, low_price, volume)
+    
+    # Display the predicted future stock price
+    st.write(future_price)
--- prediction.py
+++ prediction.py
@@ -1,25 +1,38 @@
+# Import necessary libraries
 import os
-import langchain
 from langchain.agents import create_csv_agent
 from langchain.llms import OpenAI
 from dotenv import load_dotenv
 
+# Load environment variables from a .env file
 load_dotenv()
 
+# Retrieve the OpenAI API key from environment variables
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 
-
+# Define the path to the CSV training data
 train_data = "example_data.csv"
 
+# Define a function to predict stock prices
 def predict_stock(open_price, high_price, low_price, volume):
+    
+    # Initialize the OpenAI language model (GPT-3)
     llm = OpenAI(api_key=openai_api_key, temperature=0)
+    
+    # Create an agent that uses the language model and CSV data for predictions
     agent = create_csv_agent(llm, train_data, verbose=True)
 
-    input_string = "Train and read the csv file and predict the future price of a stock based on Open Price: {}, High Price: {}, Low Price: {}, and Volume: {} if the values are in dataset, if not then predict a value learned from the data"
-    
+    # Define the input string for the prediction task
+    input_string = (
+        "Train and read the CSV file and predict the future price of a stock based on "
+        "Open Price: {}, High Price: {}, Low Price: {}, and Volume: {} if the values are "
+        "in the dataset, if not then predict a value learned from the data"
+    )
+
+    # Format the input string with the provided stock data
     input_string = input_string.format(open_price, high_price, low_price, volume)
-    
+
+    # Run the agent to generate a prediction
     prediction = agent.run(input_string)
+    
     return prediction
-
-
